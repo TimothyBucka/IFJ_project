d84985d647121e610a0c0f6907082c5918adaa63
DanielMacura <danny.macura@gmail.com>
2022-12-06 22:51:50 +0100

    another one bites the dust

--- symtable.h
+++ symtable.h
@@ -6,10 +6,10 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define TABLESIZE 193
-#define PRIME 1153
-#define P 103
-#define A 83103
+#define TABLESIZE 49157
+#define PRIME 196613            //prime number larger than TABLESIZE   
+#define P 37                    //empirical constant    http://www.cs.emory.edu/~cheung/Courses/253/Syllabus/Map/hash-func.html
+#define A 83103                 
 #define B 930721
 
 typedef struct
