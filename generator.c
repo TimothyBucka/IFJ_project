19f735f1e2b43d0fcceb1ad26bb3a975af3d0192
Tobsterr58 <tobste58@gmail.com>
2022-12-06 23:40:57 +0100

    FIXED AND OPTIMALIZED - zafajceny vykureny motorest kde sme este pred minutou chceli jest

--- generator.c
+++ generator.c
@@ -7,93 +7,27 @@
 #include "chararray.h"
 
 
-	// (FUNCTION_SUBSTR);
-	// (FUNCTION_CHR);
-    // (FUNCTION_ORD);
-    // (FUNCTION_STRLEN);
-    // // (FUNCTION_READS); //read string
-    // // (FUNCTION_READI); //read int
-    // // (FUNCTION_READF); //read float
-    // // (FUNCTION_WRITE);
-    // // (FUNCTION_FLOATVAL);
-    // // (FUNCTION_INTVAL);
-    // // (FUNCTION_STRVAL);
-
-
-	// ("# Start of program");
-
-	// (".IFJcode22");
-
-	// ("DEFVAR GF@%input_prompt");
-	// ("MOVE GF@%input_prompt string@?\\032");
-
-	// ("DEFVAR GF@%tmp_op1");
-	// ("DEFVAR GF@%tmp_op2");
-	// ("DEFVAR GF@%tmp_op3");
-
-	// ("DEFVAR GF@%exp_result");
-
-	// ("JUMP $$main");
-
 
     void start_of_generator(){
         printf(START);
 
-        gen_fun_reads();
-        gen_fun_readi();
-        gen_fun_readf();
-        gen_fun_write();
-        gen_fun_floatval();
-        gen_fun_intval();
-        gen_fun_strval();
-        gen_fun_strlen();
-        gen_fun_substr();
-        gen_fun_ord();
-        gen_fun_chr();
+        generate_buildin_functions();
 
-        printf("LABEL $$main\n");
+        printf("LABEL $main\n");
     }
 
-    void gen_fun_reads(){
-        printf(FUNCTION_READS); 
+    void generate_buildin_functions(){
+        printf(FUNCTION_READS);
+        printf(FUNCTION_READI);
+        printf(FUNCTION_READF);
+        printf(FUNCTION_WRITE);
+        printf(FUNCTION_FLOAT_VALUE);
+        printf(FUNCTION_INT_VALUE);
+        printf(FUNCTION_STRING_VALUE);
+        printf(FUNCTION_STRING_LENGTH);
+        printf(FUNCTION_SUBSTRING);
+        printf(FUNCTION_ORD);
+        printf(FUNCTION_CHR);
     }
 
-    void gen_fun_readi(){
-        printf(FUNCTION_READI); 
-    }
-
-    void gen_fun_readf(){
-        printf(FUNCTION_READF); 
-    }
-
-    void gen_fun_write(){
-        printf(FUNCTION_WRITE); 
-    }
-
-    void gen_fun_floatval(){
-        printf(FUNCTION_FLOAT_VALUE); 
-    }
-
-    void gen_fun_intval(){
-        printf(FUNCTION_INT_VALUE); 
-    }
-
-    void gen_fun_strval(){
-        printf(FUNCTION_STRING_VALUE); 
-    }
-
-    void gen_fun_strlen(){
-        printf(FUNCTION_STRING_LENGTH); 
-    }
-
-    void gen_fun_substr(){
-        printf(FUNCTION_SUBSTRING); 
-    }
-
-    void gen_fun_ord(){
-        printf(FUNCTION_ORD); 
-    }
-
-    void gen_fun_chr(){
-        printf(FUNCTION_CHR); 
-    }
+// TODO zavolame write zakazdym z danym termom a mame hotovy write
